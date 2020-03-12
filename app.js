3d88c0fa4f9f3d66e3dcb1c7b8a2f73562a5e9d1
jjeonginpark <jjeonginpark@gmail.com>
2020-03-12 15:17:40 +0900

    output 설정

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ const sanitizeHtml = require('sanitize-html');
 const url = require('url');
 
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); // client단에서 자바스크립트 경로 찾아주는 역할.
 console.log(__dirname);
 
 app.get('/', (req, res) => {
     fs.readFile('./index.html', (err, data) => {
+        if (err) throw err;
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    });
+});
+
+app.get('/about', (req, res) => {
+    fs.readFile('./about.html', (err, data) => {
         if (err) throw err;
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(data);
